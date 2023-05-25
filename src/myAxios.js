df0712cad511b3be5553d1b4bfa3feee96fdc64e
David Koffi Amegayibor <Amegah40@gmail.com>
2023-05-25 02:05:20 +0000

    minor change

--- src/myAxios.js
+++ src/myAxios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import https from 'https';
 
 const myAxios = axios.create({
-  baseURL: "https://dave-scandiweb.000webhostapp.com/",
+  baseURL: "https://dave-scandiweb.000webhostapp.com",
   httpsAgent: new https.Agent({
     rejectUnauthorized: false
   })
